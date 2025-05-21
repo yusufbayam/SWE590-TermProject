e3fa85c3516a4024a1d23c12606068400380eb03
YusufBayam <yusufbayam@gmail.com>
2025-05-21 19:49:55 +0300

    Remove health check annotations from ingress configuration and delete health check URLs from service1 and service2 routing

--- service1/hello_app/urls.py
+++ service1/hello_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import HelloWorldView, health_check
 
 urlpatterns = [
-    path('', health_check, name='health_check'),
     path('hello/', HelloWorldView.as_view(), name='hello_world'),
 ]
--- service2/evening_app/urls.py
+++ service2/evening_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import GoodEveningView, health_check
 
 urlpatterns = [
-    path('', health_check, name='health_check'),
     path('evening/', GoodEveningView.as_view(), name='good_evening'),
 ]
