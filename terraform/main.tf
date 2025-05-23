5849dce8d0168c0c7637107260d1e1e0309ab4ec
YusufBayam <yusufbayam@gmail.com>
2025-05-23 23:48:02 +0300

    Remove cached Terraform state files now ignored by .gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ provider "kubernetes" {
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
-
+/*
 # --- Frontend Deployment ---
 resource "kubernetes_deployment" "frontend" {
   metadata {
@@ -260,3 +260,4 @@ resource "kubernetes_ingress_v1" "main" {
     }
   }
 }
+*/
